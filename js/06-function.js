561ca32debf6ce488b944def0e98017fc9c3cc1a
XSYTO <arcebasovas@gmail.com>
2022-03-31 12:03:08 +0300

    function

--- js/06-function.js
+++ js/06-function.js
@@ -26,3 +26,55 @@ console.log('Jonas:', jonasVidurkis);
 
 const onaVidurkis = vidurkis(ona);
 console.log('Ona:', onaVidurkis);
+
+
+/*
+Funkcija gauna varda ir turi grazinti sakini:
+"Vardo pirma raide yra [V] ir vardas sudarytas is [5] raidziu."
+*/
+
+function vardoAnalize(name) {
+    return `Vardo "${name}"pirma raide yra ${name[0]} ir vardas sudarytas is ${name.length} raidziu.`
+}
+
+const petroVardas = vardoAnalize('Petras');
+console.log(petroVardas);
+
+const onaVardas = vardoAnalize('Ona');
+console.log(onaVardas);
+
+/*
+Funkscija gauna du skaicius ir turi grazinti lygti su sprendimu, pvz.:
+2 + 2 = 4
+*/
+
+function suma(a, b) {
+    return `${a} + ${b} = ${a + b}`;
+}
+
+const s1 = suma(2, 3);
+console.log(s1);
+
+const s2 = suma(0.1, 0.2);
+console.log(s2);
+
+const s3 = suma(-88, -11);
+console.log(s3);
+
+/*
+
+desimtainis skaicius
+0           00
+1           01
+2           10
+3           011
+4           100
+5           101
+6           110
+7           111
+8           1000
+
+0.55         5,5*10^-1     [sveika, desimataine, laipsnis]
+0.0714      7.14*10^-2  [7, 14, -2]
+
+*/
