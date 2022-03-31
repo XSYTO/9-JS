f2e1c8febe848e1970f3fd66d7effde70cc155b0
XSYTO <arcebasovas@gmail.com>
2022-03-31 13:28:16 +0300

    for

--- js/05-if.js
+++ js/05-if.js
@@ -4,6 +4,9 @@ IF
 Sablonai:
 if () {}
 if () {} else {}
+if () {} else if {}
+if () {} else if {} else
+if () {} else if {} else {} .... else if{}
 
 Palyginimo operatoriai:
 - visi: >, <, <=, =>, ==, !=, ===
@@ -31,3 +34,32 @@ if (a === b) {
 } else{
     console.log('A ne lygu B');
 }    
+
+console.log('-------');
+
+const day = 10;
+
+if (day === 1) {
+    console.log('pirmadienis');
+} else if (day === 2) {
+    console.log('antradienis');
+} else if (day === 3) {
+    console.log('treciadienis');
+} else if (day === 4) {
+    console.log('ketvirtadienis');
+} else if (day === 5) {
+    console.log('penktadienis');
+} else if (day === 6) {
+    console.log('sestadienis');
+} else if (day === 7) {
+    console.log('sekmadienis');
+} else {
+    console.log('tokios dienos savaiteje nera');
+}
+
+console.log('-------');
+const akiuSpalva = 'melyna';
+
+if (akiuSpalva === 'zalia') {
+    console.log('Zaliaakiai yra zalciai!');
+}
